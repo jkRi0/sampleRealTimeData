13ee95f3c40985f38c1260e5f5fb5c94cabd08ec
justine ken <rioverosjustine92@gmail.com>
2025-03-24 11:07:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-let temp={"p_2b2AYss":"AIzaSyDPJs","AIzaSyDPJs":"qvoVtxOfmh","qvoVtxOfmh":"spUx7HNVAj","spUx7HNVAj":"p_2b2AYss",}const api = temp.p_2b2AYss+temp.qvoVtxOfmh+temp.AIzaSyDPJs+temp.spUx7HNVAj;
+let temp={"p_2b2AYss":"AIzaSyDPJs","AIzaSyDPJs":"qvoVtxOfmh","qvoVtxOfmh":"spUx7HNVAj","spUx7HNVAj":"p_2b2AYss",};const api = temp.p_2b2AYss+temp.qvoVtxOfmh+temp.AIzaSyDPJs+temp.spUx7HNVAj;
